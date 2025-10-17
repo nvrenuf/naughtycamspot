fbe112f9b76312c571d5bf43d9894f71a85c68dc
Lee Cuevas <leecuevas@gmail.com>
2025-10-16 23:58:03 -0600

    Configure Astro for site and Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false
-    })
-  ]
+  site: 'https://naughtycamspot.com',
+  base: '/',
+  integrations: [tailwind({ applyBaseStyles: false })],
 });
---
+++ astro.config.pages.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  site: 'https://leecuevasowner.github.io/naughtycamspot',
+  base: '/naughtycamspot/',
+});
