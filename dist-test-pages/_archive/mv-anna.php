7e101b486f487d90c7b8d3a350736085bdcb8634
Lee Cuevas <leecuevas@gmail.com>
2025-11-21 01:15:17 -0700

    removed dist folders

--- dist-test-pages/_archive/mv-anna.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/affiliates.php';
-
-$tracking = go_extract_tracking_params();
-
-go_send_redirect(go_build_mv_anna_url($tracking['subid']));
